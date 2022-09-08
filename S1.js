c07562f29424b71b633c44bac65c38f5b8597a8e
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-08 14:23:35 -0500

    Objetos como variables

---
+++ S1.js
@@ -0,0 +1,13 @@
+// Los objetos son variables en javascript
+
+let person = "David";
+
+const persons = {
+    firtsname : "John",
+    lastname : "Suarez",
+    john : 50
+}
+
+
+console.log(person);
+console.log(persons);
